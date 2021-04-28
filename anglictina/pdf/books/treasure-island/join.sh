efd9de6dd315f9a6044ec50367b6f9ae4386c9f3
Adam Jenča <jenca.adam@gmail.com>
2021-04-28 19:12:15 +0200

    scripts

--- anglictina/pdf/books/treasure-island/join.sh
+++ anglictina/pdf/books/treasure-island/join.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 for i in chapter* ;do
+	echo $i;
 	cd $i;
 	pdfjam page?.pdf worksheet.pdf -o chapter.pdf;
 	qpdf --rotate=-90 chapter.pdf --replace-input;
 
-	cd /home/adam/skola/anglictina/pdf/books/oliver-twist ;
+	cd ..;
 	pwd;
 	done
 pdfjam chapter*/chapter.pdf -o book.pdf
--- anglictina/pdf/books/treasure-island/print-worksheet.sh
+++ anglictina/pdf/books/treasure-island/print-worksheet.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-lpr chapter$1/worksheet.pdf
+if [ -z "$1" ];then
+  echo "print-worksheet: not enough args" ;exit 1;fi
+if [ -z "$2" ];then 
+ 	array=$(dir);
+  value=$1;
+  if [[ " ${array[@]} " =~ " ${value} " ]]; then
+     lpr chapter$1/worksheet.pdf;
+  exit $?;fi
+
+     echo "print-worksheet: no such chapter";exit 1
+  else
+  echo "print-worksheet: too much args";
+  exit 1;fi
