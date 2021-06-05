c5bd2b24ca869489cc04f2c95a209d009206d182
Adam Jenča <jenca.adam@gmail.com>
2021-06-05 19:09:32 +0200

    an

--- vyv/anim/animate.js
+++ vyv/anim/animate.js
@@ -1,4 +1,23 @@
 var jazdaSimul;
+function scene3(){
+	$("#tabula").hide(0);
+	$("#flag").hide(0);
+	$("body").css({"background-image":"url(\"assets/bg2.jpg\")","background-repeat":"no-repeat"})}
+	
+
+function scene2(){
+	
+	$("body").append("<img src=\"assets/tabularasa.png\" id=\"tabula\" >")
+	$("body").append("<img src=\"assets/flag.png\" id=\"flag\" >")
+
+	$("body").css({"background-image":"url(\"assets/hd2.jpg\")","background-repeat":"no-repeat"})
+	$("#tabula").css({"opacity":"90%","transform":"translate(1300px,300px) scale(0.1)"})
+	$("#flag").css({"position":"absolute",display:"block","left":"700px","top":"0px","transform":"scale(0.2) translate(700px,-900px)",opacity:"70%"})
+	$("#tabula").transition({opacity:100,y:1500,x:2300,scale:5,duration:49974})
+	$("#flag").transition({opacity:100,x:-5000,y:1500,duration:50000,scale:1})
+	setTimeout(scene3,15000)
+	setTimeout(function(){var arranat = new Audio("music/arranat.mp3");arranat.play()},5000)
+}	
 function scene1(){
 	$("#knight").hide(0);
 	$("#poz").hide(0);
@@ -10,23 +29,35 @@ function scene1(){
 	 	setTimeout(function(){$("#textik").fadeOut(300,function(){
 	$("#textik").text("Zabil som toľko príšer, že ma to už nebaví počítať!").fadeIn(300)})},3000)
 	setTimeout(function(){$("#textik").fadeOut(300,function(){
-	$("#textik").text("A práve smerujem...").fadeIn(300)})},3000)
+	$("#textik").text("A teraz smerujem ...").fadeIn(300)})},6000)
 	setTimeout(function(){$("#textik").fadeOut(300,function(){
-	$("#textik").text("Ku kráľovstvu Arranat.").fadeIn(300)})},3000)
+	$("#textik").text("Ku kráľovstvu Arranat.").fadeIn(300)})},9000)
 	setTimeout(function(){$("#textik").fadeOut(300,function(){
-	$("#textik").text("Počul som, že tam majú").fadeIn(300)})},3000)
-
+	$("#textik").text("Počul som, že tam majú").fadeIn(300)})},12000)
+	setTimeout(function(){$("#textik").fadeOut(300,function(){
+	$("#textik").html("<span  style=\"font-size:40pt\">Obrovského</span> draka").fadeIn(300)})},15000)
+	setTimeout(function(){$("#textik").fadeOut(300,function(){
+	$("#textik").html("Veď viete ako to chodí...").fadeIn(300)})},18000)
+	setTimeout(function(){$("#textik").fadeOut(300,function(){
+	$("#textik").html("Princeznú za ženu a pol kráľovstva k tomu!").fadeIn(300,function (){var evil = new Audio('music/evil.mp3');evil.play()})})},21000)
+	setTimeout(function(){$("body").css('background-color','white');$("#hlava").hide(100,function(){console.log("eisdjieajdmsx")});$("#textik").hide(100,scene2)},25000)
 	}
+
+
+
+
+
 function start(){
 var timer;
 var datTimer;
+
 datTimer=setInterval(function(){try{audio.stop()} catch {} ;var audio = new Audio('music/knock.mp3');
-	audio.volume=1;
+	audio.volume=0.5;
 	audio.play();
 $("#datel-im").rotate({animateTo:36,callback:function(){try{audio.stop()} catch {} ;var audio = new Audio('music/knock.mp3');
 	audio.volume=1
 	audio.play();
-$("#datel-im").rotate({animateTo:0})}})},2100);
+$("#datel-im").rotate({animateTo:0})}})},3000);
 $('#knight').animate(
 {left:"500px"},
 5000,
@@ -59,5 +90,5 @@ function(){
 		
 
 setTimeout(function(){clearInterval(timer),$("#pumpky").fadeOut(500);	var audio = new Audio('music/dead.wav');audio.volume=0.3
-	audio.play();$("#knight").attr("src","assets/knight.gif");$('#knight').animate({left:"2000px"},2000,scene1).hide(0);$("#datel-im").hide(0)},5000);
+	audio.play();$("#knight").attr("src","assets/knight.gif");$('#knight').animate({left:"2000px"},2000,scene1).hide(0);$("#datel-im").fadeOut(300)},5000);
 })}
--- vyv/anim/run.js
+++ vyv/anim/run.js
@@ -2,7 +2,7 @@ $('body').append($('<button>SPUSTI ANIMACIU</button>'))
 $('button').click(function(){
 var theme = new Audio('music/theme.wav');
 theme.volume=0.3;
-//theme.play();
+theme.play();
 $('body').append($('<img src="assets/forest.png" id="poz" style="min-width:100%;bottom:0px;position:fixed;">'))
 $('body').append($('<img src="assets/knight.gif" id="knight" style="bottom:100px;left:0px;transform:scale(0.6);position:absolute;">'))
 $('body').append($('<img src="assets/datel.ico" id="datel-im" style="top:0px;left:0px;position:absolute;">'))
