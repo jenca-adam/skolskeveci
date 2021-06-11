2a7e521afa8204b427c6db1f72bf34fea2c53c65
Adam Jenča <jenca.adam@gmail.com>
2021-06-11 14:18:00 +0200

    Fri Jun 11 02:18:00 PM CEST 2021

--- anglictina/zing/tribes/ayoreo/gallery.js
+++ anglictina/zing/tribes/ayoreo/gallery.js
@@ -31,7 +31,7 @@ function reload(){
 	$("#desc").fadeOut(function(){$(this).load(descs[ii])}).delay(100).fadeIn()
 }
 $("#image").html('<img id="inner" src="'+images[ii]+'" style="margin-left:auto;max-height:65%;max-width:50%;transform:scale(2)">')
-$("#image").append('<p id="desc" style="top:70%;margin:auto;position:fixed;text-align:center;"></p>')
+$("#image").append('<p id="desc" style="top:70%;margin:auto;text-align:center;"></p>')
 reload();
 $(".bullet").click(function(){
 		if (animated){return;}
